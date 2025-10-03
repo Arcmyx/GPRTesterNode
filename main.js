f260a0aed8aee003eef939065567a2a63c4e07fd
alkadienePhoton <atharvna12@gmail.com>
2025-10-03 08:34:06 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+let visualizerWindow, joystickWindow;
+
+function createWindows() {
+  visualizerWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  joystickWindow = new BrowserWindow({
+    width: 400,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  visualizerWindow.loadFile("index.html");
+  joystickWindow.loadFile("joystick.html");
+}
+
+app.whenReady().then(() => {
+  createWindows();
+
+  ipcMain.on("vectorCommand", (event, vec) => {
+    console.log("[main.js] Received vectorCommand:", vec);
+    visualizerWindow.webContents.send("vectorCommand", vec);
+    console.log("[main.js] Sent vectorCommand to visualizerWindow:", vec);
+  });
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindows();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
